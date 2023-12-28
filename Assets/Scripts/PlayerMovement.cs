58eb27ba973a9a0495575f96e191fe81c581e352
HiBAdam <ahitinbialus@gmail.com>
2023-12-28 13:51:55 +0200

    Initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody rb;
+    [SerializeField] private float speed;
+    private int count;
+    public float slow;
+    [SerializeField] private TMP_Text countText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCount();
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3 (moveHorizontal,0.0f,moveVertical);
+        rb.AddForce(movement*speed);
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count += 1;
+            SetCount();
+        }
+    }
+    void SetCount ()
+    {
+        countText.text = "Collected: " + count.ToString();
+    }
+}
